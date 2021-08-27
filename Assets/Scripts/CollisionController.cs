131d49c06ea2cbe849d0ab72f053eb5f11139567
Rockstrip <artes5kgc@gmail.com>
2021-08-27 15:21:44 +0300

    First commit

---
+++ Assets/Scripts/CollisionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+    public class CollisionController : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            EndGame();
+        }
+
+        private void EndGame()
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
